07e0aaff03dad60f1f071012f2d9905e2b2c6afd
209322 <209322@CARBON.CTI.P.LODZ.PL>
2019-05-09 17:08:22 +0200

    Lab 1.2 Display my GithubID - SzymonGrzelak

--- src/com/jamniki/Main.java
+++ src/com/jamniki/Main.java
@@ -2,6 +2,8 @@ package com.jamniki;
 
 public class Main {
     public static void main(String[] args){
+
         System.out.println("Jamniki Team Leader - SzymonGrzelak");
+        System.out.println("SzymonGrzelak");
     }
 }
