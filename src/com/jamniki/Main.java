e7b4c4f8ee4bbb471bcf5ea36b955eda01130f97
209457 <209457@edu.p.lodz.pl>
2019-05-09 17:04:12 +0200

    Zad2 Print Github id

--- src/com/jamniki/Main.java
+++ src/com/jamniki/Main.java
@@ -3,5 +3,6 @@ package com.jamniki;
 public class Main {
     public static void main(String[] args){
         System.out.println("Jamniki Team Leader - SzymonGrzelak");
+        System.out.println("209457");
     }
 }
