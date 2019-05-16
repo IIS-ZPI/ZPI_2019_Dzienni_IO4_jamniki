005b73f6b40151c3d90fe048c91bead6d1458fa3
209412 <45198261+209412@users.noreply.github.com>
2019-05-16 08:18:44 -0700

    Revert "Merge pull request #19 from IIS-ZPI/develop_Piotrek1"
    This reverts commit d7bb0e2e38a5596686f3b03d17bc602ece01daa5, reversing
    changes made to 4804f65af4d39c23f82c54543768c3daa0d17737.

--- src/com/jamniki/IArithmeticsMult.java
+++ src/com/jamniki/IArithmeticsMult.java
@@ -1,6 +1,5 @@
 package com.jamniki;
-//komentarz3
+
 public interface IArithmeticsMult {
-    double Multiplication(double A,double B);//komentarz2
+    double Multiplication(double A,double B);
 }
-//komentarz1
