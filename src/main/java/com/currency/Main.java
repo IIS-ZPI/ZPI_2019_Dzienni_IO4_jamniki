4281ee30017b4ec07b42194ec3f73277de14413b
monika1710 <m.odartus@gmail.com>
2019-06-11 18:54:42 +0200

    Add tests to assets

--- src/main/java/com/currency/Main.java
+++ src/main/java/com/currency/Main.java
@@ -1,9 +1,12 @@
 package com.currency;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Scanner;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Main {
     private static String[] CURRENCY_CODES = {"USD", "AUD", "CAD", "EUR", "HUF", "CHF", "GBP", "JPY", "CZK", "DKK", "NOK", "SEK", "XDR"};
@@ -59,12 +62,10 @@ public class Main {
         if(functionality.equals("1")) {
             oneCurrencyCalculator();
         } else if(functionality.equals("2")) {
-            //two currencies
+            doubleCurrencyCalculator();
         } else{
             System.out.println("Wront input: " + functionality);
         }
-
-
     }
 
     public static void oneCurrencyCalculator() {
@@ -97,8 +98,152 @@ public class Main {
         System.out.println("\nCurrency analize for: "+periodValue.toLowerCase());
         System.out.println("\n\nCurrency: "+codeValue.toUpperCase());
         System.out.println(currencyCalculator.toString());
+
+        saveToCSV(currencyCalculator, periodMap, codeValue, periodValue);
     }
 
+    public static void doubleCurrencyCalculator(){
+        NbpApiAdapter nbpApiAdapter = new NbpApiAdapter();
+        XmlParser xml = new XmlParser();
+        CurrencyCalculator currencyCalculator1 = new CurrencyCalculator();
+        CurrencyCalculator currencyCalculator2 = new CurrencyCalculator();
 
+        System.out.println("\nType currency symbol of the first currency: \t" + Arrays.toString(CURRENCY_CODES));
+        Scanner sc = new Scanner(System.in);
+        String codeValue1 = sc.nextLine();
+        if(!verifyCurrency(codeValue1.toUpperCase()))
+            return;
+
+        System.out.println("\nType currency symbol of the second currency: \t" + Arrays.toString(CURRENCY_CODES));
+        String codeValue2 = sc.nextLine();
+        if(!verifyCurrency(codeValue2.toUpperCase()))
+            return;
+
+        System.out.println("\nSelect analize period: \t" + Arrays.toString(PERIOD_CODES));
+        String periodValue = sc.nextLine();
+        if(!verifyPeriod(periodValue.toLowerCase()))
+            return;
+
+        String url1 = nbpApiAdapter.buildUrl(codeValue1);
+        String url2 = nbpApiAdapter.buildUrl(codeValue2);
+        String rawXml1 = nbpApiAdapter.requestDataFromServer(url1);
+        String rawXml2 = nbpApiAdapter.requestDataFromServer(url2);
+
+        xml.parseData(rawXml1);
+        LinkedHashMap periodMap1 = getPeriodHashMap(periodValue.toLowerCase(), xml);
+        ArrayList periodValues1 = convertToArrayList(periodMap1);
+        currencyCalculator1.calculateAll(periodValues1);
+
+        xml.parseData(rawXml2);
+        LinkedHashMap periodMap2 = getPeriodHashMap(periodValue.toLowerCase(), xml);
+        ArrayList periodValues2 = convertToArrayList(periodMap2);
+        currencyCalculator2.calculateAll(periodValues2);
+
+        System.out.println("\nCurrency analize for: "+periodValue.toLowerCase());
+        System.out.println("\n\nCurrency: "+codeValue1.toUpperCase());
+        System.out.println(currencyCalculator1.toString());
+        System.out.println("\n\nCurrency: "+codeValue2.toUpperCase());
+        System.out.println(currencyCalculator2.toString());
+
+        saveToCSV2(currencyCalculator1, currencyCalculator2, periodMap1, periodMap2, codeValue1, codeValue2, periodValue);
+    }
+
+    public static void saveToCSV(CurrencyCalculator currencyCalculator, LinkedHashMap periodMap, String codeValue, String periodValue) {
+
+        Set<Map.Entry<String, Float>> set = periodMap.entrySet();
+        String filename = "exportedData/dataNBP-" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Timestamp(System.currentTimeMillis())) + "-" + codeValue + "-" + periodValue + ".csv";
+        String str = "";
+        String dominant = "";
+
+        if (currencyCalculator.dominantValue == null) {
+            dominant = "The dominant does not occur" + ";";
+        } else {
+            for (Object value : currencyCalculator.dominantValue) {
+                dominant = dominant + String.valueOf(value).replace('.', ',') + ";";
+            }
+        }
+
+        try  {
+            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(filename));
+            str = str + periodValue + ";" + codeValue + ";" + "\n" + "\n"
+                    + "Median" + ";" + String.valueOf(currencyCalculator.medianValue).replace('.', ',') + ";" + "\n"
+                    + "Dominant" + ";" + dominant + "\n"
+                    + "Standard Deviation" + ";" + String.valueOf(currencyCalculator.standardDeviationValue).replace('.', ',') + ";" + "\n"
+                    + "Coefficient Of Variation" + ";" + String.valueOf(currencyCalculator.coefficientOfVariationValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Grew" + ";" + String.valueOf(currencyCalculator.howManyDaysItGrewValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Dropping" + ";" + String.valueOf(currencyCalculator.calculateDroppingDaysValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Was Constant" + ";" + String.valueOf(currencyCalculator.howManyDaysItWasConstantValue).replace('.', ',') + ";" + "\n" + "\n";
+
+            for(Map.Entry<String, Float> entry : set) {
+                str = str + entry.getKey() + ";" + String.valueOf(entry.getValue()).replace('.', ',') + ";" + "\n";
+            }
+
+            dataOutputStream.write(str.getBytes("UTF-8"));
+
+            dataOutputStream.close();
+        }catch(FileNotFoundException ioe) {
+            System.out.println("File not found!");
+        }catch(IOException e){
+            System.out.println("jest abrdzo zle!");
+        }
+    }
+
+    public static void saveToCSV2(CurrencyCalculator currencyCalculator1, CurrencyCalculator currencyCalculator2, LinkedHashMap periodMap1, LinkedHashMap periodMap2, String codeValue1, String codeValue2, String periodValue) {
+
+        Set<Map.Entry<String, Float>> set1 = periodMap1.entrySet();
+        Set<Map.Entry<String, Float>> set2 = periodMap2.entrySet();
+        String filename = "exportedData/dataNBP-" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Timestamp(System.currentTimeMillis())) + "-" + codeValue1 + "-" + periodValue + ".csv";
+        String str = "";
+        String dominant1 = "";
+        String dominant2 = "";
+
+        if (currencyCalculator1.dominantValue == null) {
+            dominant1 = "The dominant does not occur" + ";";
+        } else {
+            for (Object value : currencyCalculator1.dominantValue) {
+                dominant1 = dominant1 + String.valueOf(value).replace('.', ',') + ";";
+            }
+        }
+
+        if (currencyCalculator2.dominantValue == null) {
+            dominant2 = "The dominant does not occur" + ";";
+        } else {
+            for (Object value : currencyCalculator2.dominantValue) {
+                dominant2 = dominant2 + String.valueOf(value).replace('.', ',') + ";";
+            }
+        }
+
+        try {
+            System.out.println("To tu");
+            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(filename));
+            System.out.println("To tam");
+            str = str + periodValue + ";" + codeValue1 + ";" + codeValue2  + ";" + "\n" + "\n"
+                    + "Median" + ";" + String.valueOf(currencyCalculator1.medianValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.medianValue).replace('.', ',') + ";" + "\n"
+                    + "Dominant" + ";" + dominant1 + dominant2 + "\n"
+                    + "Standard Deviation" + ";" + String.valueOf(currencyCalculator1.standardDeviationValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.standardDeviationValue).replace('.', ',') + ";" + "\n"
+                    + "Coefficient Of Variation" + ";" + String.valueOf(currencyCalculator1.coefficientOfVariationValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.coefficientOfVariationValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Grew" + ";" + String.valueOf(currencyCalculator1.howManyDaysItGrewValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.howManyDaysItGrewValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Dropping" + ";" + String.valueOf(currencyCalculator1.calculateDroppingDaysValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.calculateDroppingDaysValue).replace('.', ',') + ";" + "\n"
+                    + "How Many Days It Was Constant" + ";" + String.valueOf(currencyCalculator1.howManyDaysItWasConstantValue).replace('.', ',') + ";" + String.valueOf(currencyCalculator2.howManyDaysItWasConstantValue).replace('.', ',') + ";" + "\n" + "\n";
+
+            str = str + codeValue1 + ";" + "\n";
+            for(Map.Entry<String, Float> entry : set1) {
+                str = str + entry.getKey() + ";" + String.valueOf(entry.getValue()).replace('.', ',') + ";" + "\n";
+            }
+
+            str = str + "\n" + codeValue2 + ";" + "\n";
+            for(Map.Entry<String, Float> entry : set2) {
+                str = str + entry.getKey() + ";" + String.valueOf(entry.getValue()).replace('.', ',') + ";" + "\n";
+            }
+
+            dataOutputStream.write(str.getBytes("UTF-8"));
+
+            dataOutputStream.close();
+        }catch(FileNotFoundException ioe) {
+            System.out.println("File not found!");
+        }catch(IOException e){
+            System.out.println("jest bardzo zle!");
+        }
+    }
 
 }
--- src/test/java/com/currency/CurrencyCalculatorTest.java
+++ src/test/java/com/currency/CurrencyCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.currency;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -9,12 +10,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CurrencyCalculatorTest {
 
+    private ArrayList arrayList;
     private CurrencyCalculator currencyCalculator;
     private XmlParser xmlClass;
 
@@ -31,6 +36,54 @@ public class CurrencyCalculatorTest {
         when(nbpApiAdapter.requestDataFromServer("quarter")).thenReturn(getXmlFile("quarter.xml"));
         when(nbpApiAdapter.requestDataFromServer("half_year")).thenReturn(getXmlFile("half_year.xml"));
         when(nbpApiAdapter.requestDataFromServer("year")).thenReturn(getXmlFile("year.xml"));
+    }
+    @Test public void calculateDroppingDaysWeek() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("week"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 4);
+    }
+
+    @Test public void calculateDroppingDaysTwoWeeks() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("two_weeks"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 7);
+    }
+
+    @Test public void calculateDroppingDaysMonth() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("month"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 14);
+    }
+
+    @Test public void calculateDroppingDaysQuarter() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("quarter"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 34);
+    }
+
+    @Test public void calculateDroppingDaysHalfYear() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("half_year"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 59);
+    }
+
+    @Test
+    public void calculateDroppingDaysYear() {
+        xmlClass.parseData(nbpApiAdapter.requestDataFromServer("year"));
+        LinkedHashMap hashMap = xmlClass.getAllParsed();
+        arrayList = Main.convertToArrayList(hashMap);
+
+        assertEquals(currencyCalculator.calculateDroppingDays(arrayList), 129);
     }
 
     private String getXmlFile(String filename) throws IOException {
